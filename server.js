d81f286d6817378ef35e95d8947749d067563b39
Michael Hamm <mikejameshamm@gmail.com>
2019-03-11 16:50:51 -0700

    Add newrelic to proxy

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
+require('newrelic');
 const express = require('express');
 const cors = require('cors');
 const compression = require('compression');
 const path = require('path');
 const favicon = require('serve-favicon')
+const proxy = require('http-proxy-middleware')
+
 
 const app = express();
 
@@ -18,8 +21,6 @@ app.get('/products/:id', (req, res) => {
   res.sendFile(`${__dirname}/public/index.html`);
 });
 
-app.get('*', (req, res) => {
-  res.redirect('/products/1');
-})
+app.use('/api', proxy({target: 'http://127.0.0.1:3000'}))
 
-app.listen(80);
+app.listen(3001);
